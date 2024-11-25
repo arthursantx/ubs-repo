48db004eeabed4c6a3f70ed69a24d49be8556f82
JaumVituu <78977800+JaumVituu@users.noreply.github.com>
2024-11-25 18:10:32 -0300

    Arthur21

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
-
+  nomeCompleto = "Nome do usuário";
+  nomeSocial = "Nome social";
+  nomeMae = '';
+  nomePai = '';
+  dataNascimento = '';
+  sexo = 'masculino';
+  nacionalidade = '';
+  municipioNascimento = '';
+  racaCor = 'branca';
+  frequentaEscola = '';
+  escolaridade = '';
+  situacaoFamiliar = 'solteiro';
+  vinculo = '';
+  cadastro = '';
+  deficiencia = 'nenhuma';
+  celular = '';
+  residencial = '';
+  comercial = '';
+  contato = '';
+  email = '';
+  enderecoOrigem = '';
+  endereco = '';
+  cpf = '';
+  identidade = '';
 }
