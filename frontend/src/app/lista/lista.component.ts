6c1546b1dd211b7d1d2dcb612be2e131f687e791
samaraAlmeid <samara.silva28@fatec.sp.gov.br>
2025-02-17 18:17:59 -0300

    lista atualizada

--- frontend/src/app/lista/lista.component.ts
+++ frontend/src/app/lista/lista.component.ts
@@ -1,28 +1,69 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importando o FormsModule
 import { Paciente } from '../model/paciente';
-import { PacienteService } from '../service/paciente.service'
+import { PacienteService } from '../service/paciente.service';
+ 
 @Component({
   selector: 'app-lista',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule], // Adicionando FormsModule aqui
   templateUrl: './lista.component.html',
-  styleUrl: './lista.component.css',
+  styleUrls: ['./lista.component.css'],
   providers: [PacienteService]
 })
 export class ListaComponent {
-  mensagem: String="";
+  mensagem: string = '';
   pacientes: Paciente[] = [];
-
+  selecionado: Paciente | null = null;
+ 
   constructor(private service: PacienteService) {
     this.listarPacientes();
   }
-
+ 
   listarPacientes() {
     this.service.listar().subscribe({
-      next: (data) => {this.pacientes = data;},
-      error: (msg) => {this.mensagem = "occoreu erro"}
+      next: (data) => {
+        this.pacientes = data;
+      },
+      error: (msg) => {
+        this.mensagem = 'Ocorreu um erro ao carregar os pacientes.';
+      }
     });
   }
-
+ 
+  editar(paciente: Paciente) {
+    this.selecionado = { ...paciente };
+  }
+ 
+  salvar() {
+    if (this.selecionado) {
+      this.service.salvar(this.selecionado).subscribe({
+        next: (response) => {
+          this.mensagem = 'Paciente atualizado com sucesso!';
+          this.listarPacientes();
+          this.cancelarEdicao();
+        },
+        error: (err) => {
+          this.mensagem = 'Erro ao salvar alterações.';
+        }
+      });
+    }
+  }
+ 
+  cancelarEdicao() {
+    this.selecionado = null;
+  }
+ 
+  excluir(paciente: Paciente) {
+    this.service.excluir(paciente.id).subscribe({
+      next: () => {
+        this.mensagem = 'Paciente excluído com sucesso!';
+        this.listarPacientes();
+      },
+      error: (erro) => {
+        this.mensagem = 'Erro ao excluir o paciente.';
+      }
+    });
+  }
 }
--- frontend/src/app/service/paciente.service.ts
+++ frontend/src/app/service/paciente.service.ts
@@ -2,22 +2,34 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Paciente } from '../model/paciente';
-
+ 
 @Injectable({
   providedIn: 'root',
 })
-
-
+ 
+ 
 export class PacienteService {
-
+ 
   constructor(private http: HttpClient) { }
-
+ 
   public salvar(obj: Paciente): Observable<Paciente> {
     return this.http.post<Paciente>('http://localhost:8090/pacientes', obj);
   }
-
+ 
   public listar(): Observable<Paciente[]> {
     return this.http.get<Paciente[]>('http://localhost:8090/pacientes');
   }
-
+ 
+  public deletar(obj: Paciente): Observable<Paciente> {
+    return this.http.post<Paciente>('http://localhost:8090/pacientes', obj);
+  }
+ 
+  public excluir(id: number): Observable<void> {
+    return this.http.delete<void>(`http://localhost:8090/pacientes/${id}`);
+  }
+ 
+  public atualizar(obj: Paciente): Observable<Paciente> {
+    return this.http.put<Paciente>(`http://localhost:8090/pacientes/${obj.id}`, obj);
+  }
+ 
 }
